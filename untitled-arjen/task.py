7294f4d582a2a707b37d3263acec1da8816f7d28
Arjen Swartsenburg <11778614+ArjenSwartsenburg@users.noreply.github.com>
2026-06-01 16:49:17 +0200

    untitled-arjen

---
+++ untitled-arjen/task.py
@@ -0,0 +1,21 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+print('All packages imported successfully!')
+
